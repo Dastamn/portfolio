ba24ab067e164e25e8dfcd7b25f5bb19c053023e
Dastamn <mezhoudi.nazim@gmail.com>
2021-10-29 22:50:54 +0200

    Added sass support

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,6 @@
 const React = require('react');
 const Layout = require('./src/components/Layout').default;
+require('./src/styles/main.scss');
 
 exports.wrapPageElement = ({ element, props }) => (
   <Layout {...props}>{element}</Layout>
